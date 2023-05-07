ac741bf4263d3dc397c215645feb0bbf8f3f8dc2
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-07 21:03:09 +0200

    stateflow contrairement à livedata ne suit pas le cyle de vie doit le mettre dans le scope

--- app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
+++ app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.viewmodeldemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -17,9 +19,13 @@ class MainActivity : AppCompatActivity() {
         viewModelFactory = MainActivityViewModelFactory(125)
         viewModel = ViewModelProvider(this,viewModelFactory).get(MainActivityViewModel::class.java)
 
-        viewModel.totalData.observe(this, Observer {
+        /*viewModel.totalData.observe(this, Observer {
             binding.resultTextView.text = it.toString()
-        })
+        })*/
+        // stateflow contrairement à livedata ne suit pas le cyle de vie doit le mettre dans le scope
+        lifecycleScope.launchWhenCreated {
+
+        }
 
         binding.insertButton.setOnClickListener {
             viewModel.setTotal(binding.inputEditText.text.toString().toInt())
--- app/src/main/java/com/example/viewmodeldemo/MainActivityViewModel.kt
+++ app/src/main/java/com/example/viewmodeldemo/MainActivityViewModel.kt
@@ -1,19 +1,28 @@
 package com.example.viewmodeldemo
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MainActivityViewModel(startingTotal : Int) : ViewModel() {
-    private var total = MutableLiveData<Int>()
+   /* private var total = MutableLiveData<Int>()
     val totalData : LiveData<Int>
-        get() = total
+        get() = total*/
+// même principe que live data on peur lire la valeur observer mais pas la midifié
+
+private val _flowTotal = MutableStateFlow<Int>(0)
+    val flowTotal : StateFlow<Int> = _flowTotal
+    //get() = _flowTotal
+
 
     init {
-        total.value = startingTotal
+        //total.value = startingTotal
+        _flowTotal.value = startingTotal
     }
 
     fun setTotal(input:Int){
-        total.value =(total.value)?.plus(input)
+       // total.value =(total.value)?.plus(input)
+        _flowTotal.value = (_flowTotal.value).plus(input)
     }
 }
